7fc5286ec5b3ff3a90b27fe88da2aa3367aec15f
JonLuca DeCaro <jdecaro@usc.edu>
2019-07-06 10:54:36 -0700

    Initial working version to save traffic stats

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Churning-Traffic-Stats
+
 Data and code for quarterly analysis of traffic to /r/churning
+
+## Usage
+
+Create a file `account.py` with the following format:
+
+```python
+mod_username = "username"
+mod_password = "password"
+mod_script_id = "id"
+mod_script_secret = "secret"
+```
+
+## File layout
+
+The arrays in the "days" array is laid out like: [epoch time, uniques, pageviews, subscriptions]
+
+The arrays in both the "hours" and "months" arrays are laid out like: [epoch time, uniques, pageviews]
---
+++ traffic_stats.py
@@ -0,0 +1,53 @@
+import datetime
+import glob
+import json
+import os
+
+import praw
+
+from account import *
+
+
+def save_traffic(traffic):
+    now = datetime.datetime.now()
+    filename = str(now.today()) + '.json'
+    with open('data/' + filename, 'w') as traffic_file:
+        traffic_file.write(json.dumps(traffic))
+        traffic_file.close()
+
+
+def retrieve_traffic_stats():
+    r = praw.Reddit(client_id=mod_script_id, client_secret=mod_script_secret, user_agent="churning_stats",
+                    username=mod_username,
+                    password=mod_password)
+
+    sub = r.subreddit('churning')
+    traffic = sub.traffic()
+    save_traffic(traffic)
+
+
+def create_csv_from_json(filename):
+    csv_filename = filename.replace('json', 'csv')
+    if os.path.isfile(csv_filename):
+        return
+    with open(filename) as json_contents:
+        data = json.loads(json_contents.read())
+
+    day_csv = 'unix_time,unique,page_views,subscriptions\n'
+    for day in data.day:
+        day_csv += ','.join(map(str, day)) + '\n'
+
+    with open(csv_filename, 'w') as csv:
+        csv.write(day_csv)
+        csv.close()
+    print(data)
+
+
+def create_csvs_from_json():
+    json_files = glob.glob('data/*.json')
+    for file in json_files:
+        create_csv_from_json(file)
+
+
+if __name__ == "__main__":
+    create_csvs_from_json()
