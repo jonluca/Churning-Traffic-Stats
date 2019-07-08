69c6574df885a0aff863f491e795d6b3f56219d3
JonLuca DeCaro <jdecaro@usc.edu>
2019-07-07 21:00:12 -0700

    Added some stats, fixed CSV creation

--- traffic_stats.py
+++ traffic_stats.py
@@ -43,7 +43,7 @@ def create_csv_from_json(filename):
         csv.close()
 
     # Write hour
-    hour_csv = 'unix,uniques,pageviews'
+    hour_csv = 'unix,uniques,pageviews\n'
     for hour in data['hour']:
         hour_csv += ','.join(map(str, hour)) + '\n'
     with open(csv_filename.replace('.csv', '_hour.csv'), 'w') as csv:
@@ -51,19 +51,41 @@ def create_csv_from_json(filename):
         csv.close()
 
     # Write month
-    month_csv = 'unix,uniques,pageviews'
+    month_csv = 'unix,uniques,pageviews\n'
     for month in data['month']:
         month_csv += ','.join(map(str, month)) + '\n'
     with open(csv_filename.replace('.csv', '_month.csv'), 'w') as csv:
         csv.write(month_csv)
         csv.close()
-    print(data)
+
+
+def create_stats(filename):
+    with open(filename) as json_contents:
+        data = json.loads(json_contents.read())
+
+    subs_60_days = 0
+    least = {'amount': 99999, 'day': None}
+    most = {'amount': -1, 'day': None}
+    for day in data['day']:
+        subs_60_days += day[3]
+        if day[3] < least['amount'] and day[3]:
+            least['amount'] = day[3]
+            least['day'] = day[0]
+        if day[3] > most['amount']:
+            most['amount'] = day[3]
+            most['day'] = day[0]
+
+
+    print(f'{subs_60_days} new subscriptions in last {len(data["day"])} days')
+    print(f'The most subscriptions, {most["amount"]}, were on {datetime.datetime.utcfromtimestamp(most["day"]).strftime("%Y-%m-%d")}')
+    print(f'The least subscriptions, {least["amount"]}, were on {datetime.datetime.utcfromtimestamp(least["day"]).strftime("%Y-%m-%d")}')
 
 
 def create_csvs_from_json():
     json_files = glob.glob('data/*.json')
     for file in json_files:
         create_csv_from_json(file)
+        create_stats(file)
 
 
 if __name__ == "__main__":
